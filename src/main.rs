02972c3f60146d3f13f2613bd88a519e50c6545c
Nathaniel Daniel <nathaniel.daniel23@outlook.com>
2021-07-07 18:07:59 -0700

    Move util functions to skylight (#1)

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
-mod util;
-
-use crate::util::get_special_folder_path;
-use crate::util::get_user_name;
-use crate::util::ConstantSpecialItemIdList;
 use std::ffi::OsString;
 use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
 
 fn main() -> std::io::Result<()> {
-    let desktop_folder_path = get_special_folder_path(ConstantSpecialItemIdList::Desktop, false)
+    let desktop_folder_path = skylight::get_known_folder_path(skylight::FolderId::Desktop)
+        .ok()
+        .map(|p| PathBuf::from(p.as_os_string()))
+        .or_else(|| {
+            skylight::get_special_folder_path(skylight::ConstantSpecialItemIdList::Desktop, false)
+        })
         .or_else(|| {
             let user_profile = std::env::var_os("USERPROFILE").or_else(|| {
-                let user = get_user_name().ok()?;
+                let user = skylight::get_user_name().ok()?;
                 Some(OsString::from(format!(
                     "C:/Users/{}/Desktop",
                     user.to_string_lossy()
@@ -22,7 +22,7 @@ fn main() -> std::io::Result<()> {
         })
         .expect("failed to locate desktop_folder_path");
 
-    let nul = desktop_folder_path.canonicalize()?.join("NUL"); 
+    let nul = desktop_folder_path.canonicalize()?.join("NUL");
     let mut file = File::create(nul)?;
     writeln!(file, "pranked")?;
     Ok(())
